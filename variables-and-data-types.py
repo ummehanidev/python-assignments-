9ad681178d227c7713092ddf768afd4546cfc507
Umme Hani <hani281824@gmail.com>
2026-06-30 23:49:18 +0500

    Add Assignment 2 - Variables and Data Types

---
+++ variables-and-data-types.py
@@ -0,0 +1,19 @@
+print("Python Assignment 2 : Variables and Data Types")
+print("        STUDENT PROFILE        ")
+
+name = "Ummehani"
+age = 16
+city = "Karachi"
+course = "FS Foundations (JS/Py)"
+
+print("My name is", name)
+print("Type:", type(name))
+
+print("My age is", age)
+print("Type:", type(age))
+
+print("My city is", city)
+print("Type:", type(city))
+
+print("My course is", course)
+print("Type:", type(course))
